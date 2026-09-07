1e2c988b0a1dd9228fe3e4da9343adb8f76ace2f
betaanoiar1-gif <betaanoiar1@gmail.com>
2026-09-07 18:52:37 +0100

    Add asynchronous Solana RPC client

---
+++ app/rpc.py
@@ -0,0 +1,35 @@
+import asyncio
+import aiohttp
+
+class RpcError(RuntimeError):
+    pass
+
+class RpcClient:
+    def __init__(self, url: str):
+        self.url = url
+        self._id = 0
+
+    async def call(self, method: str, params=None):
+        self._id += 1
+        payload = {"jsonrpc":"2.0","id":self._id,"method":method,"params":params or []}
+        timeout = aiohttp.ClientTimeout(total=20)
+        async with aiohttp.ClientSession(timeout=timeout) as s:
+            async with s.post(self.url, json=payload) as r:
+                r.raise_for_status()
+                data = await r.json()
+        if data.get("error"):
+            raise RpcError(str(data["error"]))
+        return data.get("result")
+
+    async def get_transaction(self, signature: str):
+        return await self.call("getTransaction", [signature, {
+            "encoding":"jsonParsed",
+            "commitment":"confirmed",
+            "maxSupportedTransactionVersion":0,
+        }])
+
+    async def get_account_info(self, pubkey: str):
+        return await self.call("getAccountInfo", [pubkey, {"encoding":"base64"}])
+
+    async def get_multiple_accounts(self, pubkeys):
+        return await self.call("getMultipleAccounts", [pubkeys, {"encoding":"base64"}])
